9691c0a3bcbd58de709d7dd78be26add665ffdaa
adet-astro <adetia.marhadi@astronauts.id>
2023-03-23 11:14:31 +0700

    init project

---
+++ src/main/java/com/github/adetiamarhadi/springsecuritysscbrewery/SpringSecuritySscBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.springsecuritysscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySscBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/adetiamarhadi/springsecuritysscbrewery/SpringSecuritySscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.springsecuritysscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
