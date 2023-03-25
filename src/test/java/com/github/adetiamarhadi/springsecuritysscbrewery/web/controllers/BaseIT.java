2c6a384702611c6d6ba2086d0142822cf17e0ee3
adet-astro <adetia.marhadi@astronauts.id>
2023-03-25 15:53:32 +0700

    adding test for HTTP Basic and adding permit all for index page

---
+++ src/test/java/com/github/adetiamarhadi/springsecuritysscbrewery/web/controllers/BaseIT.java
@@ -0,0 +1,45 @@
+package com.github.adetiamarhadi.springsecuritysscbrewery.web.controllers;
+
+import com.github.adetiamarhadi.springsecuritysscbrewery.repositories.BeerInventoryRepository;
+import com.github.adetiamarhadi.springsecuritysscbrewery.repositories.BeerRepository;
+import com.github.adetiamarhadi.springsecuritysscbrewery.repositories.CustomerRepository;
+import com.github.adetiamarhadi.springsecuritysscbrewery.services.BeerService;
+import com.github.adetiamarhadi.springsecuritysscbrewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+public abstract class BaseIT {
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BeerService beerService;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(springSecurity())
+                .build();
+    }
+}
